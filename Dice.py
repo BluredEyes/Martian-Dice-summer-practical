15a3db4ab0422710b0d26356b81245c899882a13
Ritorino <leigentv@mail.ru>
2024-07-03 23:50:12 +0300

    Update Dice.py

--- Dice.py
+++ Dice.py
@@ -1,10 +1,26 @@
-class Dice #lightblue 
-{ SIDES : [str]
-  ..
-  current_side_i : int(0..5)
-  ==
-  roll()
-  current_side() -> str
-  ..
-  save() -> json
-  load(json)}
+import json
+from random import randint
+class Dice():
+    SIDES = [1, 2, 3, 4, 5, 6]
+    def __init__(self, side=None):
+        if side != None and 1 <= side <= 6:
+            self.current_side = side
+        else:
+            self.roll()
+
+    def __repr__(self):
+        return str(self.current_side)
+
+    def roll(self):
+        self.current_side = self.SIDES[randint(0, 5)]
+
+    def save(self):
+        with open('data1.json', 'w') as file:
+            json.dump({"current_side":self.current_side}, file)
+        return repr(self)
+
+    @classmethod
+    def load(cls, value: str):
+        with open('data1.json', 'r') as file:
+            s = json.load(file)
+        return cls(s["current_side"])
