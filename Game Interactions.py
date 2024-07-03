fee37e1c926d87466b9a6a4fec0de190217bf1b7
Ritorino <leigentv@mail.ru>
2024-07-04 01:58:41 +0300

    Update Game Interactions.py

--- Game Interactions.py
+++ Game Interactions.py
@@ -1,11 +1,21 @@
-class GameInteractions #lightgreen {
-  ==
-  run()
-  load(json)
-  ..
-  request_players() -> [Player]
-  print_rolled_dice([Dices])
-  print_side_dice([Dices])
-  print_player_info(Player)
-}
-GameInteractions -- GameState
+from dataclasses import dataclass
+
+@dataclass
+class GameInteractions:
+    def run(self):
+        pass
+
+    def load(self, json):
+        pass
+
+    def request_players(self) -> list['Player']:
+        pass
+
+    def print_rolled_dice(self, dices: list['Dice']):
+        pass
+
+    def print_side_dice(self, dices: list['Dice']):
+        pass
+
+    def print_player_info(self, player: 'Player'):
+        pass
